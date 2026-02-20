1852b12790503a7d0decfebf82b14b98456c2e40
Rishabh Jain <2r10j5@gmail.com>
2026-02-21 02:08:14 +0530

    Enhance install script with pre-requisites and library setup
    Added pre-requisites and library installation steps for audio recording setup.

--- mic/install.sh
+++ mic/install.sh
@@ -1,7 +1,45 @@
+# NOTE!:
+#---------------------------------------------------------------------------------------------
 # No use the latest version of the RPi OS thus INMP441 does not support or dected by the RPi.
 # For the L/R connected with GPIO to GND Default channel will be the Left.
+#---------------------------------------------------------------------------------------------
 
-#  Used to enable the I2S (I2C & SPI) interface in Raspberry Pi.
+# Pre Request:
+# ---------------------------------------------
+# Upgrade & Update 
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Virtual enviroment.
+python3 -m venv env
+
+# Active the enviroment.
+source env/bin/activate
+
+# Upgrade pip if not.
+pip install --upgrade pip
+
+# Deactivation enviroment.
+deactivate
+# ---------------------------------------------
+
+# All this library will be in enviroment folder:
+#--------------------------------------------------------------------------------
+# INMP441 works via ALSA (Advanced Linux Sound Architecture) for globel.
+sudo apt-get install alsa-utils 
+
+# PyAudio library used for recording audio stream, real time audio capture.
+sudo apt install portaudio19-dev  # Globel
+# or 
+pip install pyaudio               # in env
+
+# Converting raw audio to arrays, Signal math operations.
+pip install numpy
+
+# Filtering, FFT, Digital signal processing.
+pip install scipy
+#--------------------------------------------------------------------------------
+
+# Used to enable the I2S (I2C & SPI) interface in Raspberry Pi.
 sudo raspi-config
 
 # Used to add the line dtparam=i2s=on to activate I2S in the boot configuration.
